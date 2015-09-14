29e2148d18bc897bd31c1f3879ae011da7a8e6bf
Mihai Rotaru <mihai.rotaru@gmx.com>
2015-09-14 20:38:03 +0100

    Search when user presses Enter

--- app.js
+++ app.js
@@ -123,3 +123,18 @@ GitHubApp.directive("issueDirective", function(){
     }
   }
 });
+
+// from: http://codepen.io/TheLarkInn/blog/angularjs-directive-labs-ngenterkey 
+GitHubApp.directive('dlEnterKey', function() {
+  return function(scope, element, attrs) {
+    element.bind("keydown keypress", function(event) {
+      var keyCode = event.which || event.keyCode;
+      if (keyCode === 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs.dlEnterKey);
+        });
+        event.preventDefault();
+      }
+    });
+  };
+});
