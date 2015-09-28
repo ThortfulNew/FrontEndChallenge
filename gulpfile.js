a660ebd222d55d88ae62d610a4512882337f7aa2
Albert Abril <albert.abril@gmail.com>
2015-09-28 17:15:50 +0200

    adding basic gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,9 @@ var gulp = require('gulp');
 var requireDir = require('require-dir');
 var tasks = requireDir('./gulp');
 var browserSync = require('browser-sync');
+
+gulp.task('build', tasks.build);
+gulp.task('loop', tasks.loop);
+gulp.task('browser', tasks.browser);
+
+gulp.task('default', ['browser']);
