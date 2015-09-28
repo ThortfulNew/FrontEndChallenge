76f37211297a7bb41ec055e2a78d3af7bc33cd8a
Albert Abril <albert.abril@gmail.com>
2015-09-28 17:19:54 +0200

    browser-sync config 2

--- gulp/browser.js
+++ gulp/browser.js
@@ -2,9 +2,14 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
 module.exports = function(){
-    var watchFiles = [];
+    var watchFiles = [
+       'public/**'
+    ];
 
     var browserConfig = {
+        server: {
+            baseDir: './public'
+        }
     };
 
     browserSync.init(watchFiles, browserConfig);
