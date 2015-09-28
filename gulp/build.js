b719144e2bc5fc9eb6c9bcc90089cb97a139f4ff
Albert Abril <albert.abril@gmail.com>
2015-09-28 18:16:56 +0200

    src/stylus -> dist/css ;)

--- gulp/build.js
+++ gulp/build.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
 
-module.exports = function(){
-  var paths = [ './src/**'];
+
+var build = {};
+
+build.copy = function(){
+  var paths = [
+    '!src/stylus/',  // except src/stylus
+    '.src/**'      // copy all files
+  ];
   return gulp.src(paths).pipe(gulp.dest('./dist/'));
 };
+
+build.stylus = function(){
+   var paths = [ 'src/stylus/main.styl'];
+   return gulp.src(paths).pipe(stylus()).pipe(gulp.dest('./dist/css'));
+};
+
+module.exports.copy = build.copy;
+module.exports.stylus = build.stylus;
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var requireDir = require('require-dir');
 var tasks = requireDir('./gulp');
 var browserSync = require('browser-sync');
 
-gulp.task('build', tasks.build);
+gulp.task('build:copy', tasks.build.copy);
+gulp.task('build:stylus', tasks.build.stylus);
+gulp.task('build', ['build:copy','build:stylus']);
+
 gulp.task('loop', tasks.loop);
 gulp.task('browser', tasks.browser);
 
